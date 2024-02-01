40cd0f62a4d0e2e25134502c8baee21bb6ac0d55
BMJ845 <158430350+BMJ845@users.noreply.github.com>
2024-02-01 20:06:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 function pickItem() {
-    const items = ['아이템1', '아이템2', '아이템3', '아이템4'];
-    const randomIndex = Math.floor(Math.random() * items.length);
-    const selectedItem = items[randomIndex];
-    
+    const items = ['1티어', '2티어', '3티어', '4티어'];
+    const probabilities = [1, 9, 20, 70];
+
+    const randomValue = Math.random() * 100;
+    let selectedItem = '';
+
+    if (randomValue < probabilities[0]) {
+        selectedItem = items[0];
+        document.getElementById('itemImage').src = 'item1.jpg';
+    } else if (randomValue < probabilities[0] + probabilities[1]) {
+        selectedItem = items[1];
+        document.getElementById('itemImage').src = 'item2.jpg';
+    } else if (randomValue < probabilities[0] + probabilities[1] + probabilities[2]) {
+        selectedItem = items[2];
+        document.getElementById('itemImage').src = 'item3.jpg';
+    } else {
+        selectedItem = items[3];
+        document.getElementById('itemImage').src = 'item4.jpg';
+    }
+
     const resultText = document.getElementById('resultText');
     resultText.innerHTML = `뽑힌 아이템: ${selectedItem}`;
 }
