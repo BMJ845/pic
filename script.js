56ef87f96488de1c32c2d92eb861cf510a24a56a
BMJ845 <158430350+BMJ845@users.noreply.github.com>
2024-02-01 20:12:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,112 @@
 function pickItem() {
-    const items = ['1티어', '2티어', '3티어', '4티어'];
-    const probabilities = [1, 9, 20, 70];
+    
+   
+const items = ['1티어', '2티어', '3티어', '4티어'];
+    
+   
+const probabilities = [1, 9, 20, 70];
 
-    const randomValue = Math.random() * 100;
+    
+
+   
+
+
+const randomValue = Math.random() * 100;
     let selectedItem = '';
 
-    if (randomValue < probabilities[0]) {
-        selectedItem = items[0];
-        document.getElementById('itemImage').src = 'item1.jpg';
-    } else if (randomValue < probabilities[0] + probabilities[1]) {
-        selectedItem = items[1];
-        document.getElementById('itemImage').src = 'item2.jpg';
-    } else if (randomValue < probabilities[0] + probabilities[1] + probabilities[2]) {
-        selectedItem = items[2];
-        document.getElementById('itemImage').src = 'item3.jpg';
-    } else {
-        selectedItem = items[3];
-        document.getElementById('itemImage').src = 'item4.jpg';
+    
+
+   
+if (randomValue < probabilities[0]) {
+        selectedItem = items[
+        selectedItem = items
+
+        selectedItem
+
+       
+0];
+        
+       
+document.getElementById('itemImage').src = 'item1.jpg';
+    } 
+   
+else if (randomValue < probabilities[0] + probabilities[1]) {
+        selectedItem = items[
+        selectedItem = items[
+
+        selectedItem =
+
+       
+1];
+        
+       
+document.getElementById('itemImage').src = 'item2.jpg';
+    } 
+    }
+else if (randomValue < probabilities[0] + probabilities[1] + probabilities[2]) {
+        selectedItem = items[
+        selectedItem = items
+
+        selectedItem =
+
+        selectedI
+
+        selec
+
+        se
+
+       
+
+    
+
+ 
+2];
+        
+        documen
+
+     
+document.getElementById('itemImage').src = 'item3.jpg';
+    } 
+    } e
+
+   
+else {
+        selectedItem = items[
+        selectedItem = ite
+
+        selectedItem
+
+        select
+
+        
+
+   
+3];
+        
+        docu
+
+        
+
+   
+document.getElementById('itemImage').src = 'item4.jpg';
     }
 
-    const resultText = document.getElementById('resultText');
-    resultText.innerHTML = `뽑힌 아이템: ${selectedItem}`;
+    
+    }
+
+  
+
+  
+const resultText = document.getElementById('resultText');
+    resultText.
+    resultText
+
+    resultTe
+
+    resul
+
+    re
+
+   
+innerHTML = `뽑힌 아이템: ${selectedItem}`;
 }
